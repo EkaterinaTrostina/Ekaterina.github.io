e5f06f6e8bf82164031f4ebc95cf95c4939ad83e
Katya <katena.trostina@mail.ru>
2021-01-12 18:01:59 +0300

    layout

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ gulp.task('server', function() {
         }
     });
     gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
 gulp.task('styles', function(){
@@ -34,6 +35,7 @@ gulp.task('watch', function(){
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
     gulp.watch('src/sass/*.+(scss|sass|css').on('change', browserSync.reload);
     gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
 gulp.task('html', function(){
