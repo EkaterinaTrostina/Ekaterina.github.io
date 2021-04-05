724afca037f0bdaa296456292b77000712e323dc
Katya <katena.trostina@mail.ru>
2021-04-05 18:55:51 +0300

    media

--- src/js/script.js
+++ src/js/script.js
@@ -87,15 +87,29 @@ window.addEventListener('DOMContentLoaded', function(){
     };
 
 
-    //hamburger
-    // const hamburgerBtn = document.querySelector('.hamburger'),
-    //       hamburgerMenu = document.querySelector('.hamburger__wrap');
-    // function toggleHamburger(trigger, menu){
-    //     trigger.addEventListener('click', function(){
-    //         menu.classList.toggle = 'active';
-    //     });
-    // }
-    // toggleHamburger(hamburgerBtn, hamburgerMenu);
+    // hamburger
+    const hamburgerBtn = document.querySelector('.hamburger'),
+          hamburgerMenu = document.querySelector('.hamburger__wrap'),
+          hamburgerClose = document.querySelector('.hamburger__close'),
+          menuItem = document.querySelectorAll('.hamburger__link');
+    function toggleHamburger(trigger, menu, menuItem, close){
+        trigger.addEventListener('click', function(){
+            menu.classList.add('active');
+            document.body.style.overflow = 'hidden';
+        });
+        close.addEventListener('click', function(){
+            menu.classList.remove('active');
+            document.body.style.overflow = '';
+        });
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                menu.classList.remove('active');
+                document.body.style.overflow = '';
+            })
+        })
+
+    }
+    toggleHamburger(hamburgerBtn, hamburgerMenu, menuItem, hamburgerClose);
 
 
 
