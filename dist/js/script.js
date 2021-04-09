9b969fb177bddcd8f3f8c0d19f3dd58b32d04038
Katya <katena.trostina@mail.ru>
2021-04-09 18:03:46 +0300

    validate form

--- dist/js/script.js
+++ dist/js/script.js
@@ -127,4 +127,39 @@ window.addEventListener('DOMContentLoaded', function(){
 
     toggleModal(openModal, modal, closeModal, overlay);
 
+
+
+    //validate
+    const formCards = document.querySelector('#cards-form'),
+          formQuestions = document.querySelector('#questions-form'),
+          formModal = document.querySelector('#form-modal');
+
+    function validateForm(form){
+        $(form).validate({
+            rules:{
+                name: "required",
+                phone:"required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Please specify your name",
+                phone: "Please specify your phone",
+                email: {
+                  required: "Please specify your email",
+                  email: "format: name@domain.com"
+                }
+            }
+        }); 
+        
+    }
+
+    validateForm(formCards);
+    validateForm(formQuestions);
+    validateForm(formModal);
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
 });
